d04cbaf6f734d4a1899af613881fec88f36dd232
Nicholas Genta <58099250+mbahmu25@users.noreply.github.com>
2024-03-04 14:18:58 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output:'export'
+  reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/github-pages',
+
 };
 /* module.exports = nextConfig*/
 export default nextConfig;
