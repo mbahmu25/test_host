33396542f24b472a88fa88797aa5a01d3560f7a4
Nicholas Genta <58099250+mbahmu25@users.noreply.github.com>
2024-03-04 13:42:58 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div class="width-[100px] hieght-[100px] bg-[red]">
+      <div className="width-[100px] hieght-[100px] bg-[red]">
         tes
       </div>
     </>
