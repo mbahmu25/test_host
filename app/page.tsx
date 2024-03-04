8c62041226dda99ae2bd902c06989a4316af0e71
mbahmu25 <nikogenta25@gmail.com>
2024-03-04 13:51:38 +0700

    tes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="width-[100px] hieght-[100px] bg-[red]">
+      <div className="w-[100px] h-[100px] bg-[blue]">
         tes
       </div>
     </>
